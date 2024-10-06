8b7b01716191536405b7aedfb800c189c6000025
rkshnagila <144611667+rkshnagila@users.noreply.github.com>
2024-10-06 18:00:26 +0530

    Update Question4.js

--- Question4.js
+++ Question4.js
@@ -1,1 +1,44 @@
+function deepCopy(obj) {
+    if (obj === null || typeof obj !== 'object'){
+        return obj;
+    }
+    const copy = Array.isArray(obj) ? [] : {};
+        
+      
+         for (const key in obj) {
+            if(obj.hasOwnProperty.call(obj.key)) {
+                copiedObj[key] = deepCopy(obj[key]);
+            }
+        }
+        return copiedObj;
+    }
+
+const obj = {
+  name: "John",
+  address: {
+    city: "New York",
+    zip: 10001
+  }
+};
+
+const copy = deepCopy(obj);
+console.log(copy);
+// Output: { name: "John", address: { city: "New York", zip: 10001 } }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
